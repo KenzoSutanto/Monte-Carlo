c787d2ecf988a37cb89799270e5c91ba3f8d97f4
KenzoSutanto <kenzosutanto0406@gmail.com>
2025-08-08 17:48:47 +0800

    Fixed incorrect mean annualization and volatility annualization

--- monte-carlo.py
+++ monte-carlo.py
@@ -2,17 +2,19 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-simulations = 100
-sim_days = 5
-data = (yf.download("SPLG",period="max",interval="1d",auto_adjust=False))
+
+simulations = int(input("Enter number of simulations: "))
+sim_days = int(input("Enter number of days you want to simulate: "))
+ticker = input("Enter a ticker: ")
+data = (yf.download(ticker,period="max",interval="1d",auto_adjust=False))
 
 data["Returns"] = data["Adj Close"].pct_change()
 last_price = float(data["Adj Close"].iloc[-1] )
 
 simulations_df = np.zeros((sim_days,simulations)) 
 
-sigma = data["Returns"].std() * 252
-mu = data["Returns"].mean() * np.sqrt(252)
+sigma = data["Returns"].std() * np.sqrt(252)
+mu = data["Returns"].mean() * 252
 t = 1/252
 for sim in range(simulations):
     price_list = [last_price]
@@ -26,6 +28,9 @@ print(f"The median final price is: {(np.median(final_prices)):.2f}")
 print(f"The mean final price is: {(np.mean(final_prices)):.2f}")
 
 plt.plot(simulations_df)
-plt.xlabel("Days")
-plt.ylabel("price")
+plt.title(f"Monte Carlo simulation for {ticker}")
+plt.xlabel("Days-1")
+plt.ylabel("Price")
 plt.show()
+
+#np.percentile(final_price, 5) #Shows 5th percentile, higher the better
